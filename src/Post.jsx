cce03abe650bd6b8f69e29728638a57b1bb2fc7f
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-03-30 21:17:28 -0500

    more troubleshooting

--- src/Post.jsx
+++ src/Post.jsx
@@ -22,7 +22,7 @@ const Post = () => {
     
   useEffect(() => {
     if(postData) {
-      document.querySelector("[property=og:image]").setAttribute("content", postData.cover_image_src)
+      document.querySelector('[property="og:image"]').setAttribute("content", postData.cover_image_src)
     }
   }, [postData])
 
