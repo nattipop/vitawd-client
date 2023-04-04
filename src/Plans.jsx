1f545856b203da9d510eb679fcc116eecb148bd1
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-04-04 16:00:21 -0500

    google user content

--- src/Plans.jsx
+++ src/Plans.jsx
@@ -24,7 +24,7 @@ const Plans = () => {
       plansData.map(plan => {
         return (
           <div className="col plan-div" key={plan._id}>
-            <iframe src={plan.icon_url} width="640" height="480" allow="autoplay"></iframe>
+            <image src={plan.icon_url} className="plan-icon" width="640" height="480" allow="autoplay"></image>
             <h2>{plan.plan_name} <span>{plan.price}</span></h2>
             <p>{plan.description}</p>
             <ul>
