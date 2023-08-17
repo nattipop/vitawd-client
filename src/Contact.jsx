96a403eb502088660afb60292ab88b1ea092d3aa
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 11:04:00 -0500

    color

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,8 +2,8 @@ import { Widget } from '@typeform/embed-react'
 
 const Contact = () => {
   return (
-    <div style={{alignContent: "center"}} className='container'>
-      <h3 className="header-front-page" style={{width: "60%", padding: "3vw"}}>If you have any interest in a meeting, please fill out this form.</h3>
+    <div style={{alignContent: "center", backgroundColor: "var(--website-theme)"}} className='container'>
+      <h3 className="header-front-page" style={{width: "60%", padding: "3vw", textAlign: "center"}}>If you have any interest in a meeting, please fill out this form.</h3>
       <Widget id="a5iLhJ7z" style={{ width: '100%', height: "700px" }} className="my-form" />
     </div>
   )
