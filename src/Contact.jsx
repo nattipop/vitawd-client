2365a9c0d93573b4bf592658581ad313f3637a13
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 11:11:27 -0500

    margin top

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,7 +3,7 @@ import { Widget } from '@typeform/embed-react'
 const Contact = () => {
   return (
     <div style={{alignContent: "center", backgroundColor: "var(--website-theme)", textAlign: "center", width: "100%"}}>
-      <h3 className="header-front-page" style={{width: "100%", padding: "3vw"}}>If you have any interest in a meeting, please fill out this form.</h3>
+      <h3 className="header-front-page" style={{width: "100%", padding: "3vw", marginTop: "5vw"}}>If you have any interest in a meeting, please fill out this form.</h3>
       <Widget id="a5iLhJ7z" style={{ width: '100%', height: "700px", padding: "4vw"}} className="my-form" />
     </div>
   )
