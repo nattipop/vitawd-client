618a2bc79e806bdc5be22218860f75769bec6ed7
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 10:29:14 -0500

    width

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,7 @@
 import { Widget } from '@typeform/embed-react'
 
 const Contact = () => {
-  return <Widget id="a5iLhJ7z" className="my-form" />
+  return <Widget id="a5iLhJ7z" width="50%" className="my-form" />
 }
 
 export default Contact;
