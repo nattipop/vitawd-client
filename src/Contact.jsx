7d54337a6c9dffbd506baf528bddf4679ecbe87d
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 10:54:11 -0500

    styling

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,12 @@
 import { Widget } from '@typeform/embed-react'
 
 const Contact = () => {
-  return <Widget id="a5iLhJ7z" style={{ width: '50%', height: "900px" }} className="my-form" />
+  return (
+    <div className='text-center'>
+      <h1>If you have any interest in a meeting, please fill out this form.</h1>
+      <Widget id="a5iLhJ7z" style={{ width: '50%', height: "700px" }} className="my-form" />
+    </div>
+  )
 }
 
 export default Contact;
