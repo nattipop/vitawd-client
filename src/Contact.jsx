40219aa138480b59543348880753d58d9769aa61
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 11:06:54 -0500

    container width and center

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -2,9 +2,9 @@ import { Widget } from '@typeform/embed-react'
 
 const Contact = () => {
   return (
-    <div style={{alignContent: "center", backgroundColor: "var(--website-theme)"}} className='container'>
-      <h3 className="header-front-page" style={{width: "60%", padding: "3vw", textAlign: "center"}}>If you have any interest in a meeting, please fill out this form.</h3>
-      <Widget id="a5iLhJ7z" style={{ width: '100%', height: "700px" }} className="my-form" />
+    <div style={{alignContent: "center", backgroundColor: "var(--website-theme)", textAlign: "center", width: "100%"}} className="container">
+      <h3 className="header-front-page" style={{width: "60%", padding: "3vw"}}>If you have any interest in a meeting, please fill out this form.</h3>
+      <Widget id="a5iLhJ7z" style={{ width: '100%', height: "700px", padding: "4vw"}} className="my-form" />
     </div>
   )
 }
