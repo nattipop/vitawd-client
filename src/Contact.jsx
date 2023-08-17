dc00ab1b86114438d61919a30f98f52589dc759e
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 10:26:20 -0500

    form embed

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,7 @@
+import { Widget } from '@typeform/embed-react'
+
 const Contact = () => {
-  return (
-    <div>
-      <iframe src="//embed.typeform.com/next/embed.js" data-tf-widget="a5iLhJ7z" data-tf-opacity="100" data-tf-iframe-props="title=New Client Form" data-tf-transitive-search-params data-tf-medium="snippet" style="width:100%;height:500px;" />
-    </div>
-  )
+  return <Widget id="a5iLhJ7z" style={{ width: '50%'}} className="my-form" />
 }
 
 export default Contact;
