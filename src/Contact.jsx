94dbe840edcac3974829b34f4ddeaa8d096bbdb0
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 10:33:12 -0500

    test

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,7 @@
 import { Sidetab } from '@typeform/embed-react'
 
 const Contact = () => {
-  return <Sidetab id="a5iLhJ7z" buttonText="click to open" />
+  // return <Sidetab id="a5iLhJ7z" buttonText="click to open" />
 }
 
 export default Contact;
