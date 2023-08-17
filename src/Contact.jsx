7801b775d06263999e9daea6bdd2483f28d8dce4
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-08-17 10:06:22 -0500

    iframe

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,7 +1,7 @@
 const Contact = () => {
   return (
     <div>
-      <div data-tf-widget="a5iLhJ7z" data-tf-opacity="100" data-tf-iframe-props="title=New Client Form" data-tf-transitive-search-params data-tf-medium="snippet" style="width:100%;height:500px;"></div><script src="//embed.typeform.com/next/embed.js"></script>
+      <iframe src="//embed.typeform.com/next/embed.js" data-tf-widget="a5iLhJ7z" data-tf-opacity="100" data-tf-iframe-props="title=New Client Form" data-tf-transitive-search-params data-tf-medium="snippet" style="width:100%;height:500px;" />
     </div>
   )
 }
