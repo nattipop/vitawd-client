a2342dc31bf2ead931d616bef7a993deb79060be
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2023-11-30 11:53:11 -0600

    number

--- src/components/Placeholder.jsx
+++ src/components/Placeholder.jsx
@@ -9,7 +9,10 @@ const Placeholder = () => {
           <div className="col">
             <img id="placeholder-img" src={logo} width="300px" alt="" />
           </div>
-          <h1 id="placeholder-title">Website currently under construction</h1>
+          <div className="col" id="placeholder-title">
+            <h1>Website currently under construction</h1>
+            <h3>Call (715) 642-1146 with any questions</h3>
+          </div>
         </div>
       </div>
     </div>
