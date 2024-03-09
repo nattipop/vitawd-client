541aa2a512cf45e5bc84db0f840d75ae71bb1b83
Natalie Poppe <90017469+nattipop@users.noreply.github.com>
2024-03-09 13:49:10 -0600

    more about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,30 @@
 import { useEffect } from "react";
 import "../styles/About.css";
-import "../styles/Mobile.css"
+import "../styles/Mobile.css";
 
 const About = () => {
   useEffect(() => {
     document.title = "Vita Web Design | About";
     window.scrollTo(0, 0);
-  }, [])
+  }, []);
   return (
     <div id="about-section">
       <div id="overlay"></div>
-        <div id="bible-verse">
-          <h1>
-            "Vita" (Pronounced vee • tuh) is the Latin word for "life"
-            <br/>
-            <br/>
-            "In Him was life, and the life was the light of men. The light shines in the darkness, and the darkness has not overcome it."
-            <br/>
-            <br/>
-            John 1:4-5 
-          </h1>
+      <div id="bible-verse">
+        <h1>
+          "Vita" (Pronounced vee • tuh) is the Latin word for "life"
+          <br />
+          <br />
+          In English, Vita is used to mean biography or
+          résumé. Just as the purpose of both of these is to tell a story, your website is made to tell a story: your story.
+          <br />
+          <br />
+          "In Him was life, and the life was the light of men. The light shines
+          in the darkness, and the darkness has not overcome it."
+          <br />
+          <br />
+          John 1:4-5
+        </h1>
       </div>
       <div id="about-main">
         <div className="col" id="about-header">
@@ -31,17 +36,26 @@ const About = () => {
             <img src="https://res.cloudinary.com/dawteptkh/image/upload/v1704213076/IMG_2349_roe58d.jpg" />
           </div>
           <div id="about-me-text" className="col-7">
-            <h3>I started Vita Web Design because I believe a good website is vital to promoting a business. My goal is to build a website that complements the quality of your business while catering to your specific needs.
+            <h3>
+              I started Vita Web Design because I believe a good website is <i>vital</i> to promoting a business. My goal is to build a website that
+              complements the quality of your business while catering to your
+              specific needs.
             </h3>
-            <br/>
-            <h3>I have worked with HTML, CSS and Javascript to build projects from scratch, as well as building with frameworks like <a href="https://blog.hubspot.com/website/react-js">React</a> and content management systems such as <a href="https://wordpress.org/">Wordpress.org</a>.</h3>
-            <br/>
+            <br />
+            <h3>
+              I have worked with HTML, CSS and Javascript to build projects from
+              scratch, as well as building with frameworks like{" "}
+              <a href="https://blog.hubspot.com/website/react-js">React</a> and
+              content management systems such as{" "}
+              <a href="https://wordpress.org/">Wordpress.org</a>.
+            </h3>
+            <br />
             <h3></h3>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default About;
